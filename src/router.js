85d8986b1aa7cf5cda748a43fd47eca22ac4396a
Clark Newell <williamcnewell@gmail.com>
2018-05-30 15:31:55 -0600

    update

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Goals from './views/Goals.vue'
+import Progress from './views/Progress.vue'
 
 Vue.use(Router)
 
@@ -13,9 +14,14 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
+      path: '/goals',
+      name: 'goals',
+      component: Goals
+    },
+    {
+      path: '/progress',
+      name: 'progress',
+      component: Progress
     }
   ]
 })
